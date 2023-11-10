f147484e84cc4ceac45cfda2ae6224d36a32f8fe
SEMI-SAINT-GAMES <120226332+SEMI-SAINT-GAMES@users.noreply.github.com>
2023-11-10 15:16:27 +0200

    л

--- apps/users_requests/models.py
+++ apps/users_requests/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
 from apps.users.models import UserModel
+from core.enums.cars_enum import CAR_BRANDS, CAR_BODY_TYPES
 from core.enums.regex_enum import RegEx
 from core.enums.regions_enum import REGIONS
 from core.models import BaseModel
@@ -22,21 +23,22 @@ class PartRequestModel(BaseModel):
     class Meta:
         db_table = 'car_requests'
         ordering = ['id']
-    car_brand = models.CharField(max_length=30)
+    car_brand = models.CharField(max_length=30, choices=CAR_BRANDS)
     car_model = models.CharField(max_length=30)
     year = models.IntegerField(validators=[V.MinValueValidator(1920), V.MaxValueValidator(datetime.datetime.now().year)])
-    fuel = models.CharField(max_length=30, blank=True)
-    gear_box = models.CharField(max_length=30, blank=True)
+    fuel = models.CharField(max_length=30, choices=(("Gas", "gas"), ("Diesel", "diesel"), ("LPG", "lpg")), blank=True)
+    gear_box = models.CharField(max_length=30, choices=(("Automatic", "automatic"), ("Manual", "manual"), ("Variator", "variator")), blank=True)
     car_type = models.CharField(max_length=30, blank=True)
     engine_volume = models.FloatField(validators=[V.MinValueValidator(0.2), V.MaxValueValidator(10.0)])
-    drive_unit = models.CharField(max_length=30, blank=True)
-    body_type = models.CharField(max_length=20, blank=True)
+    drive_unit = models.CharField(max_length=30, choices=(("Front", "front"), ("Rear", "rear"), ("Full", "full")), blank=True)
+    body_type = models.CharField(max_length=20, choices=CAR_BODY_TYPES, blank=True)
     region_of_car = models.CharField(max_length=20, choices=REGIONS)
     vin = models.CharField(max_length=20, blank=True, validators=[V.RegexValidator(RegEx.VIN.__str__(), RegEx.VIN.error_message())])
     about = models.CharField(max_length=10000, blank=True)
     price = models.IntegerField(validators=[V.MinValueValidator(100)])
     currency = models.CharField(max_length=3, choices=(("uah", "UAH"), ("usd","USD"), ("eur", "EUR")), blank=False, null=False)
     view_count = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=False)
     this_user_name = models.CharField(max_length=100, default=None, null=True)
     this_user_phone = models.CharField(max_length=100, blank=True)
     this_user_email = models.CharField(max_length=100, default=None, null=True)
@@ -51,4 +53,14 @@ class PartRequestModel(BaseModel):
                 self.price *= eur_rate
 
         super().save(**kwargs)
+
+    def clean(self):
+        valid_brands = dict(CAR_BRANDS).keys()
+        valid_body_types = dict(CAR_BODY_TYPES).keys()
+
+        if self.car_brand not in valid_brands:
+            raise ValidationError(f'Неправильна марка автомобіля. Виберіть з: {", ".join(valid_brands)}')
+
+        if self.body_type not in valid_body_types:
+            raise ValidationError(f'Неправильний тип кузова автомобіля. Виберіть з: {", ".join(valid_body_types)}')
     #avatar = models.ImageField(upload_to=upload_avatar_for_cars, blank=True)
